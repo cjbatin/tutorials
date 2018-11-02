1dc3b56cc284d9e223380679416687e57d056332
Srushtika <n.srushtika@gmail.com>
2018-11-02 15:10:03 +0000

    Referencing the text area to display results

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ var apiKey = '<YOUR-API-KEY>';
 var ably = new Ably.Realtime({ 
     key: apiKey
 });
+
+var metaChannel = ably.channels.get("[meta]channel.lifecycle");
+var resultArea = document.getElementById("result");
+resultArea.scrollTop = resultArea.scrollHeight;
